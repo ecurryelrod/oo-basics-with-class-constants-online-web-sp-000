bbfebd1013b5e1863afb3fa16e58a4e3f07ea3a0
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-17 17:46:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,9 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    if brand.any? 
-    BRANDS << brand
+    if BRANDS.include?(brand) 
+      BRANDS << brand
+    end
   end
 
   def cobble
